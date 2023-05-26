87f3679106799a952fc13e41c70103f4a21a5173
johanrd <johanroed@gmail.com>
2023-05-26 12:45:44 +0200

    ember install ember-cli-typescript@latest

---
+++ types/global.d.ts
@@ -0,0 +1,7 @@
+// Types for compiled templates
+declare module 'ember-typescript-reproducible-mutable-array/templates/*' {
+  import { TemplateFactory } from 'ember-cli-htmlbars';
+
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
