960178dcdf00e63290621170d3fbd8e469984558
johanrd <johanroed@gmail.com>
2023-05-26 12:51:16 +0200

    convert to app.ts, and add tracked list. No typescript errors.

--- app/app.ts
+++ app/app.ts
@@ -2,11 +2,20 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-typescript-reproducible-mutable-array/config/environment';
+import { tracked } from '@glimmer/tracking';
+
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  @tracked list : {}[] = []
+
+  pushObjectToList(object: {}) {
+    this.list.pushObject(object)
+  }
+
 }
 
 loadInitializers(App, config.modulePrefix);
